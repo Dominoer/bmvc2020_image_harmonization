b7888a73c0502559532ff7e8946898a3f6805af1
Guoqing Hao <haoguoqing02@gmail.com>
2021-02-03 23:45:18 +0900

    update download link

--- download_model.sh
+++ download_model.sh
@@ -4,7 +4,7 @@ mkdir checkpoint
 cd checkpoint
 
 echo "Downloading the harmonization model..."
-wget https://drive.google.com/file/d/1wwDMRnAUp_xrTisyzZ3XTgghp15u2Xsy/view?usp=sharing -O network.pth
+wget -r "https://docs.google.com/uc?export=download&id=1wwDMRnAUp_xrTisyzZ3XTgghp15u2Xsy" -O network.pth
 
 cd ..
 echo -e "Download finished successfully!"
